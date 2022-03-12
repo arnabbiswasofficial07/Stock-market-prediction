ac8c030f41db0eeba9ce41ab385930241c60a102
arnabbiswasofficial07 <arnabbiswasofficial07@gmail.com>
2022-03-12 15:39:38 +0530

    first commit

---
+++ constants.py
@@ -0,0 +1,9 @@
+consumer_key= '7x7w8Ti8GmMtuhvj7IXBuvZmP'
+consumer_secret= 'kpFGcKiGxP9dJtlQzZ99p2OK9HM0IjA1I8n23N40VJyctOzvkG'
+# api_key= '1FSoOy2PeHAozpyDdIOqvpmYx'
+# api_secret_key = 'sf7aoP2RZwapVKXfA1NasbESfAjZFuQnrVMWzD5jyg3EdD6wqe'
+
+access_token='1365544389498474497-97lzyZE9lroLNDzLhvW1dLS3MDKfV5'
+access_token_secret='8mF9CrBt4SvmewPZaapgL7kkh2iJus824SvhBJOvDpXlw'
+
+num_of_tweets = int(300)
